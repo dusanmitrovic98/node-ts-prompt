03957b1762cc30636679fe72ee4a70a6c279cec5
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-05 18:25:09 +0200

    Update prompt.ts

--- src/modules/prompt.ts
+++ src/modules/prompt.ts
@@ -3,3 +3,4 @@ import readline from "readline";
 async function ask(question: string): Promise<string> {
   const rl = readline.createInterface({
     input: process.stdin,
+    output: process.stdout,
